7bad50a1665e13e1a25cc4228c32c490a1fc43c6
Juan Sáez García <juambersaezgarcia@gmail.com>
2023-04-09 13:26:18 +0200

    test: dependencies

--- src/app/app.integration.spec.ts
+++ src/app/app.integration.spec.ts
@@ -6,25 +6,41 @@ import {
 } from '@angular/core/testing';
 import { Router, RouterLinkWithHref } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { clickElement, query, queryAllByDirective } from 'src/testing';
+import {
+  clickElement,
+  mockObservable,
+  query,
+  queryAllByDirective,
+} from 'src/testing';
 import { routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
+import { generateManyProducts } from './models/product.mock';
+import { ProductsService } from './services/product.service';
 
 describe('App Integration test', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
   let router: Router;
+  let productsSrv: jasmine.SpyObj<ProductsService>;
 
   beforeEach(async () => {
+    const productsSrvSpy = jasmine.createSpyObj('ProductsService', ['getAll']);
     await TestBed.configureTestingModule({
       imports: [AppModule, RouterTestingModule.withRoutes(routes)],
+      providers: [{ provide: ProductsService, useValue: productsSrvSpy }],
     }).compileComponents();
   });
 
   beforeEach(fakeAsync(() => {
+    const productsMocks = generateManyProducts(10);
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    productsSrv = TestBed.inject(
+      ProductsService
+    ) as jasmine.SpyObj<ProductsService>;
+    productsSrv.getAll.and.returnValue(mockObservable(productsMocks));
+
     fixture.detectChanges();
     // providers
     router = TestBed.inject(Router);
--- src/app/components/others/others.component.ts
+++ src/app/components/others/others.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-others',
   templateUrl: './others.component.html',
-  styleUrls: ['./others.component.scss']
+  styleUrls: ['./others.component.scss'],
 })
-export class OthersComponent{
-
+export class OthersComponent implements OnInit {
   color = 'yellow';
   text = 'Un texto';
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productsSrv: ProductsService) {}
 
+  ngOnInit(): void {
+    this.productsSrv.getAll().subscribe((items) => {
+      this.products = items;
+    });
+  }
 }
